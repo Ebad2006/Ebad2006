047eb2c00602ee855cc4740384263936e4a578e1
Ebad2006 <ebad.ali04062006@gmail.com>
2024-11-10 09:53:38 -0800

    Add files via upload

---
+++ lab 9/Q5.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void reverse(char str[], char reverse[]);
+
+int main(){
+    char str1[50], str2[50];
+    printf("Enter string: ");
+    fgets(str1, sizeof(str1), stdin);
+    reverse(str1, str2);
+    printf("Reversed string is: ");
+    puts(str2);
+}
+
+void reverse(char str[], char reverse[]){
+    int len=0;
+    for(int i=0; str[i]!='\0'; i++){
+        len++;
+    }
+
+    for(int i=0; str[i]!='\0'; i++){
+        reverse[i]=str[len-i-1];
+    }
+    return;
+}
---
+++ lab 9/Q6.C
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int max(int arr[], int n);
+int min(int arr[], int n, int max);
+
+int main(){
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    int arr[n];
+    for(int i=0; i<n; i++){
+        printf("Number %d: ", i+1);
+        scanf("%d", &arr[i]);
+    }
+    int max_num=max(arr, n);
+    printf("Maximum Number in the given array is: %d\n", max_num);
+    printf("Minimum Number in the given array is: %d", min(arr, n, max_num));
+
+}
+
+int max(int arr[], int n){
+    int max=0;
+    for(int i=0; i<n; i++){
+        if(max<arr[i]){
+            max=arr[i];
+        }
+    }
+    return max;
+}
+
+int min(int arr[], int n, int max){
+    int min=max;
+    for(int i=0; i<n; i++){
+        if(min>arr[i]) min=arr[i];
+    }
+    return min;
+}
